c27f8d7cd41b96d7da6e0bd912abaa4030b9ecbd
Graeme Brown <graemehbrown@googlemail.com>
2017-11-24 16:13:28 +0000

    pets_by_breed implemented and tested.

--- pet_shop.rb
+++ pet_shop.rb
@@ -21,3 +21,13 @@ end
 def stock_count(pet_shop)
   return pet_shop[:pets].count()
 end
+
+def pets_by_breed(pet_shop, breed_name)
+  selected_pets = []
+  for pet in pet_shop[:pets]
+    if pet[:breed] == breed_name
+      selected_pets.push(pet)
+    end
+  end
+  return selected_pets
+end
