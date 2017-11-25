07de2b1e1f0b9adcc35d65538bd7e3a4f36fa3be
Graeme Brown <graemehbrown@googlemail.com>
2017-11-25 16:18:23 +0000

    sell_pet_to_customer implemented and tested.  Homework test passes and in addition also made sell_petto_customer reduce stock level.  Added new test for stock count reduction at end of spec file.  Also fixed the remove_pet by name function as it was not working as expected.

--- pet_shop.rb
+++ pet_shop.rb
@@ -42,11 +42,8 @@ def find_pet_by_name(pet_shop, name)
 end
 
 def remove_pet_by_name(pet_shop, name)
-  for pet in pet_shop[:pets]
-    if pet[:name]== name
-      pet.delete(:name)
-    end
-  end
+  pet_to_remove = find_pet_by_name(pet_shop, name)
+  pet_shop[:pets].delete(pet_to_remove)
 end
 
 def add_pet_to_stock(pet_shop, new_pet_to_add)
@@ -69,4 +66,19 @@ def customer_can_afford_pet(customer, pet_to_buy)
   else
     return true
   end
+end
+
+def sell_pet_to_customer(pet_shop, pet_to_sell, customer)
+  #add pet to customer using function
+  add_pet_to_customer(customer, pet_to_sell)
+  #increase pets sold using function
+  increase_pets_sold(pet_shop, 1)
+  #add cash to pet shop using add or remove cash
+  price_of_pet = pet_to_sell[:price]
+  add_or_remove_cash(pet_shop, price_of_pet)
+  #remove pet from stock using remove pet by name.
+  #Note the code below isn't needed to pass the test but is included to complete function.
+  name_of_pet_sold = pet_to_sell[:name]
+  remove_pet_by_name(pet_shop, name_of_pet_sold)
+  #should also reduce customers cash by price of pet
 end
